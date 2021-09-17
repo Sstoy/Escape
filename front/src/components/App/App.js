729b6a4c7cd9a35f72ef60dbcebc42e6039c9454
Iuliia <sjlvyou@gmail.com>
2021-09-17 18:59:10 +0300

    Map Start

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import Map from "../Map/Map";
+import store from "../../redux/store"
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Provider store={store}>
+      <Router>
+
+        <Switch>
+          <Route exact path="/map">
+            <Map />
+          </Route>
+
+        </Switch>
+      </Router>
+    </Provider>
+  )
 }
 
-export default App;
+export default App
