f3dfea644347ae2bb7ca9c9a04fe72ea92932b9e
Iuliia <sjlvyou@gmail.com>
2021-09-18 16:45:58 +0300

    YandexMapComponent Done

--- front/src/components/YandexMap/YandexMap.js
+++ front/src/components/YandexMap/YandexMap.js
@@ -1,30 +1,38 @@
 import React from 'react';
+// подключение библиотеки yandex-map, на 22 строчке можно задать api-ключ
 import { YMaps, Map, Placemark, ZoomControl } from 'react-yandex-maps';
 import { useState } from 'react';
 import coords from './coords.json';
 import logo from './logo.png';
-import styles from './YandexMap.module.css';
+// стили задаем для изменения цвета фона карты, фильтр меняет также цвет лого-меток, поэтому перезаписываем стили на лого, чтобы вернуть прежний цвет лого
+import './YandexMap.css';
 
 function YandexMap() {
   const [lat, setLat] = useState(0);
   const [lng, setLng] = useState(0);
 
+  // задает центровые координаты при загрузке карты
   const mapState = {
     center: [59.91807199347784, 30.304908500000003],
-    zoom: 8
+    zoom: 9
   }
 
   return (
     <>
       {/* <YMaps query={{ apikey: '27544797-3131-4759-9f4b-54f17c827eb2&lang=ru_RU', }} version={"2.1"}> */}
       <YMaps>
-        <div className={styles.mapContainer}>
-          <Map state={mapState} >
+        <div className='mapContainer'>
+          <Map state={mapState} width='40%'
+            height='450px' >
             <ZoomControl options={{ float: 'left' }} />
             <Placemark geometry={[lat, lng]} />
 
+            {/* берет координаты из json файла, задает кастомную картинку в опциях ( Валерия Гаврилина, дом 3 корпус 1 [60.07451356399184, 30.331777499999962],
+Большая Зеленина 15 [59.96190356413901, 30.291478999999995], Проспект Пятилеток 8к1 [59.92181306419169, 30.459400999999986],
+Ленинский проспект 92к1 [59.8548470642688, 30.212112499999996]) */}
+
             {coords.map((coordinates, idx) => (
-              <Placemark className={styles.ymapsImage}
+              <Placemark
                 key={idx}
                 geometry={coordinates}
                 options={{
