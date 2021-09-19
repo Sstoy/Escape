3ff920340c7abebaddadee816de2425070133ded
Ali <alik.orujev1997@mail.ru>
2021-09-19 18:16:55 +0300

    verstka release 1 added social links,Loader

--- front/src/components/App/App.js
+++ front/src/components/App/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+// import ClubCards from "../ClubCards/ClubCards";
+// import FormReserve from "../FormReserve/FormReserve";
+import Loader from "../Loader/Loader";
+import SocialLink from "../SocialLink/SocialLink";
 // import Logo from "../Logo/Logo";
 // import MainPage from "../MainPage/MainPage";
 // import RunText from "../RunText/RunText";
@@ -7,13 +11,17 @@ function App() {
   return (
     <>
      <Router>
+     <Loader/>
         {/* <Nav/>         */}
           <Switch>
             <Route path="/" exact>  
-
+          
+              {/* <FormReserve/> */}
+              <SocialLink/>
+              {/* <ClubCards/> */}
             </Route>
             <Route path="/" exact>
-               
+            
             </Route>
             <Route path="/" exact>
               
---
+++ front/src/components/FormReserve/FormReserve.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './form.css'
+
+function FormReserve(props) {
+  return (   
+    <>
+    </>
+    
+  );
+}
+
+export default FormReserve;
