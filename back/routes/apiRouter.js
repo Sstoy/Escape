0fe97ba32c126073652b83a5bef9a0a9b93f63a9
Darya <darsolyar@icloud.com>
2021-09-21 16:14:52 +0300

    Reg - release 2-fail message and database fix

--- back/routes/apiRouter.js
+++ back/routes/apiRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const parse = require('../parsers/parser');
-const { Club, Price, News } = require('../database/models');
+const {
+  Club, Price, News, User,
+} = require('../database/models');
 
 // eslint-disable-next-line consistent-return
 router.get('/news', async (req, res) => {
@@ -77,6 +79,33 @@ router.get('/prices', async (req, res) => {
     res.json({ prices });
   } catch (error) {
     console.error(error);
+  }
+});
+
+router.post('/user', async (req, res) => {
+  const { userPhone } = req.body;
+  console.log(userPhone);
+  try {
+    const user = await User.findOne(
+      {
+        where: {
+          phone: userPhone,
+        },
+      },
+    );
+    console.log(user)
+    if (user) {
+      res.json({ message: false });
+    } else {
+      await User.create({
+        email: null,
+        phone: userPhone,
+      });
+      res.json({ message: true });
+    }
+  } catch (error) {
+    console.error(error)
+    // res.json({ message: false, reason: 'нет связи с базой данных' });
   }
 });
 
