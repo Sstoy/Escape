4d9391f68ceb806e7d8d589d6a6443508caa96a2
Darya <darsolyar@icloud.com>
2021-09-21 11:56:52 +0300

    Reg - release 1-success

--- front/src/components/Promo/Promo.js
+++ front/src/components/Promo/Promo.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import styles from './promo.module.css'
+import React, { useState } from 'react';
+import Registration from '../Registration/Registration';
+import styles from './Promo.module.css'
 
 function Promo() {
+
+  const [modal, setModal] = useState(false)
+
+  const getModal = () => {
+    setModal(true)
+  }
+
   return (
-    <div className={styles.promo}>
+    <>
+    {modal ? <Registration /> : <div className={styles.promo}>
       <span className={styles.span}>Получи два часа бесплатно! или час</span>
-      <div className={styles.button}></div>
-    </div>
+      <div className={styles.button} onClick={getModal}></div>
+    </div>}
+    </>
   );
 }
 
