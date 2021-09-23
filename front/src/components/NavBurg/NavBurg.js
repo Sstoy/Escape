cdc831411bbf63623b22933ccd458c5ff4da37f0
Iuliia <sjlvyou@gmail.com>
2021-09-23 17:26:09 +0300

    Fix Nav Burg Menu

--- front/src/components/NavBurg/NavBurg.js
+++ front/src/components/NavBurg/NavBurg.js
@@ -1,32 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
 import './NavBurg.css'
 
 function NavBurg() {
-  return ( 
-    <div className ='menunav'>
-      <input type="checkbox" id="active"/>
-      <label htmlFor="active" className='menu-btn'><i className="fas fa-bars"></i></label>
+
+  const [checkedInput, setCheckedInput] = useState(false);
+
+  return (
+    <div className='menunav'>
+      <input type="checkbox" id="active" onChange={() => setCheckedInput(!checkedInput)} checked={checkedInput ? 'checked' : null} />
+
+      <label htmlFor="active" className='menu-btn'>
+        <i className="fas fa-bars"></i></label>
       <div className='wrapper'>
         <ul>
-          <li><HashLink  to="/#mainPage">Главная</HashLink></li>
-          <li><HashLink to="/clubs#clubs">Клубы</HashLink></li>
-          <li><HashLink to="/about#aboutHeader">О нас</HashLink></li>
-          <li><HashLink to="/galery#galeryHeader">Галерея</HashLink></li>
-          <li><HashLink 
-          to="/about#footer"
-          scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}
+          <li
+            onClick={() => setCheckedInput(false)}
+          ><HashLink to="/#mainPage">Главная</HashLink></li>
+          <li
+            onClick={() => setCheckedInput(false)}
+          ><HashLink to="/clubs#clubs">Клубы</HashLink></li>
+          <li
+            onClick={() => setCheckedInput(false)}
+          ><HashLink to="/about#aboutHeader">О нас</HashLink></li>
+          <li
+            onClick={() => setCheckedInput(false)}
+          ><HashLink to="/galery#galeryHeader">Галерея</HashLink></li>
+          <li
+            onClick={() => setCheckedInput(false)}
+          ><HashLink
+            to="/about#footer"
+            scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}
           >Контакты</HashLink></li>
-      </ul>
+        </ul>
       </div>
-    <div className="content">
+      <div className="content">
         <div className="title">
+        </div>
+        <p>
+        </p>
+      </div>
     </div>
-      <p>
-      </p>
-      </div> 
-    </div>
-   
   );
 }
 
